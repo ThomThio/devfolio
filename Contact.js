9a7eecbae98992157f2baf68e9c92504897977be
Thomas Thio <thomasthio.2014@sis.smu.edu.sg>
2023-03-24 16:30:28 +0700

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,128 @@
+import React from "react";
+import userData from "@constants/data";
+
+export default function Contact() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto h-49 bg-white dark:bg-gray-800 antialiased">
+        <h1 className=" text-5xl md:text-9xl font-bold py-12 text-center md:text-left">
+          Contact
+        </h1>
+      </div>
+      <div className="relative z-10 rounded-md shadow-md bg-[#02044A] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="md:ml-4">
+            <header className="">
+              <h1 className="text-gray-50 font-semibold text-2xl">
+                Let's Chat!
+              </h1>
+            
+            </header>
+  
+            <div className="icons-container inline-flex flex-col my-3">
+            <a target="_blank" href={`https://wa.me/${userData.waphone}`}>
+            <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+            <svg
+            class="w-6 h-6 text-green-400 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512">
+            <path
+              d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
+            ></path>
+          </svg>
+                <p className="text-gray-50 font-light text-sm">
+              Whatsapp
+                </p>
+              </div>
+              </a>
+
+            
+              
+              <a target="_blank" href={`https://telegram.me/${userData.telegram}`}>
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+            <svg
+            class="w-6 h-6 text-gray-400 text-blue-400 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 496 512">
+            <path
+              d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z"
+            ></path>
+          </svg>
+                <p className="text-gray-50 font-light text-sm">
+                 Telegram                
+                </p>
+              </div>
+              </a>
+
+
+              <a target="_blank" href={`mailto:${userData.email}`}><div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+</svg>
+
+                <p className="text-gray-50 font-light text-sm">
+                 Email me: {userData.email}  
+                  
+                </p>
+              </div></a>
+
+              <a target="_blank" href={userData.calendly}>
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+</svg>
+
+                <p className="text-gray-50 font-light text-sm">
+                  Coffee? @
+                  {userData.address}
+                </p>
+              </div></a>
+
+            </div>
+            <div className="social-icons flex flex-row space-x-8">
+             
+             
+            </div>
+          </div>
+          {/* <form className="form rounded-lg bg-white p-4 flex flex-col">
+            <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+              {" "}
+              Your Name
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="name"
+            />
+            <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
+              Email
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="email"
+            />
+            <label
+              htmlFor="message"
+              className="text-sm text-gray-600 mx-4 mt-4"
+            >
+              Message
+            </label>
+            <textarea
+              rows="4"
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="message"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
+            >
+              Send Message
+            </button>
+          </form> */}
+        </div>
+      </div>
+    </section>
+  );
+}
